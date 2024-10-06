b68d54b072e90c1bfd12ee16abd9d86b40424130
Felipe Ferreira <100241185+felipeov1@users.noreply.github.com>
2024-10-06 20:33:25 -0300

    fix: files

---
+++ src/main/java/com/example/eletro_ecommerce/EletroEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.eletro_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletroEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletroEcommerceApplication.class, args);
+	}
+
+}
